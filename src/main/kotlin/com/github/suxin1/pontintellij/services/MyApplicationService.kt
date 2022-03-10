284bf8f51849731b584300bf4b171f06ef42cc9c
GitHub Action <action@github.com>
2022-03-10 03:07:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/suxin1/pontintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/suxin1/pontintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.suxin1.pontintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.suxin1.pontintellij.MyBundle
 
 class MyApplicationService {
 
